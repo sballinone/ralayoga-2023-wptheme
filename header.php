40f39b3ea2f081f909d6e82020945e719c60dbe2
Saskia Brueckner <info@saskiabrueckner.com>
2024-01-05 16:26:07 +0100

    0.1.0

--- header.php
+++ header.php
@@ -4,19 +4,18 @@
 -->
 
 <!DOCTYPE html> 
-<html> 
+<html <?php language_attributes(); ?>
 	<head> 
 		<meta charset="utf-8"> 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1"> 
-		<title>Rabea Langer Yoga</title>         
+		<meta name="viewport" content="width=device-width, initial-scale=1">         
 		
 		<?php wp_head(); ?>
 		
 	</head>     
-	<body <?php //body_class($class); ?>>
+	<body <?php body_class($class); ?>>
 	
-		<?php //wp_body_open(); ?>
+		<?php wp_body_open(); ?>
 		
 		
 		<?php 
